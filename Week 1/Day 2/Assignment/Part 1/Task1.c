d18a2bc0993a4b7b7d5f4ad6629d3c5e32388e8c
Yasseen Ahmed <Yasseen.ahmed2005@gmail.com>
2026-08-08 23:42:55 +0300

    Task 1 - Part 1

---
+++ Week 1/Day 2/Assignment/Part 1/Task1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num & 1)
+        printf("LSB of %d is set(1).\n", num);
+    else
+        printf("LSB of %d is unset (0).\n", num);
+
+    return 0;
+}
