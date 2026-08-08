40086e11ef2d75f40946477eab20f6343bca2951
Yasseen Ahmed <Yasseen.ahmed2005@gmail.com>
2026-08-09 01:00:34 +0300

    Task 4 - Part 1

---
+++ Week 1/Day 2/Assignment/Part 1/Task4.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    // set nth bit
+    signed int num = 0;
+    unsigned int bit = 0;
+
+    printf("Enter any number: ");
+    scanf("%d", &num);
+
+    printf("Enter nth bit to set (0-31): ");
+    scanf("%u", &bit);
+
+    while (bit > 31)
+    {
+        printf("Invalid input. Please enter a number between 0 and 31: ");
+        scanf("%u", &bit);
+    }
+
+    num = num | (1 << bit);
+
+    printf("Number after setting bit %u: %d\n", bit, num);
+
+    return 0;
+}
