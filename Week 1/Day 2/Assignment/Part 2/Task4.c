166b1d865d8c7ac4e8b4bba4785ca64ba3f6f70f
Yasseen Ahmed <Yasseen.ahmed2005@gmail.com>
2026-08-09 12:44:44 +0300

    Task4 - Part3

---
+++ Week 1/Day 2/Assignment/Part 2/Task4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{ // profit or loss
+
+    unsigned int costPrice = 0;
+    unsigned int sellingPrice = 0;
+    unsigned int result = 0;
+
+    printf("Enter cost price: ");
+    scanf("%d", &costPrice);
+
+    printf("Enter selling price: ");
+    scanf("%d", &sellingPrice);
+
+    if (sellingPrice > costPrice)
+    {
+        result = sellingPrice - costPrice;
+        printf("Profit = %d\n", result);
+    }
+    else if (costPrice > sellingPrice)
+    {
+        result = costPrice - sellingPrice;
+        printf("Loss = %d\n", result);
+    }
+    else
+    {
+        printf("No Profit No Loss\n");
+    }
+
+    return 0;
+}
